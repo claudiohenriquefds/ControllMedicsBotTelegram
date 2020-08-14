d93949894256b3637ffab71f3fc56b811a1beffc
claudiohenriquefds <hacksboy@hotmail.com>
2020-08-14 19:39:44 -0300

    add new var enviroment

--- knexfile.js
+++ knexfile.js
@@ -28,8 +28,9 @@ module.exports = {
     },
   
     production: {
-      client: 'postgresql',
+      client: 'pg',
       connection: {
+        host: process.env.DB_HOST,
         database: process.env.DB_NAME,
         user:     process.env.DB_USER,
         password: process.env.DB_PASSWORD
